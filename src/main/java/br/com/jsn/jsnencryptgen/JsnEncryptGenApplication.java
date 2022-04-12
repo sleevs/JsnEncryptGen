7f38be8bc8a65e0c3786aa658ad55dc966124312
Jeison Muniz <jsnprogramer@gmail.com>
2022-04-11 23:02:13 -0300

    first deploy

---
+++ src/main/java/br/com/jsn/jsnencryptgen/JsnEncryptGenApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsn.jsnencryptgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsnEncryptGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsnEncryptGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jsn/jsnencryptgen/JsnEncryptGenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jsn.jsnencryptgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsnEncryptGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
